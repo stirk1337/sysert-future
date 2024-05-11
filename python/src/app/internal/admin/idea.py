5a5ea09ab613eb33a98a07285b536aed64087535
Ramil Mavliutov <stirk2281337@gmail.com>
2024-05-11 12:11:25 +0500

    admin improve

--- python/src/app/internal/admin/idea.py
+++ python/src/app/internal/admin/idea.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
+from django.utils.html import format_html
 
 from app.internal.models.idea import Idea, Tag, IdeaTag
 
@@ -13,13 +14,21 @@ class IdeaAdmin(admin.ModelAdmin):
     inlines = [
         IdeaTagInline,
     ]
-    exclude = ('tags',)  # Exclude tags field as it's managed through the inline
+    exclude = ('tags',)
 
     def tags_list(self, obj):
         return ", ".join([tag.title for tag in obj.tags.all()])
-    tags_list.short_description = "Tags"
+
+    tags_list.short_description = "Теги"
+
+    def image_tag(self, obj):
+        return format_html(f'<img src="{obj.image} height=500" width=200/>')
+
+    image_tag.short_description = 'Картинка'
+
+    list_display = ["pk", "title", "description", "image", "tags_list", "image_tag"]
 
 
 @admin.register(Tag)
 class TagAdmin(ModelAdmin):
-    pass
+    list_display = ["pk", "title"]
