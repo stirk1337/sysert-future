9da246824701668be24349cd33ae64f65e635c50
Ramil Mavliutov <stirk2281337@gmail.com>
2024-05-11 14:46:26 +0500

    cors

--- python/src/config/settings.py
+++ python/src/config/settings.py
@@ -29,6 +29,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG_VALUE") == 'TRUE'
 
 ALLOWED_HOSTS = ["*"]
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = ['*']
 CSRF_TRUSTED_ORIGINS = ["https://sysert-ai.ru"]
 # Application definition
 
