20e420a813403d21604638df8c6f529187fcc1c8
Ramil <stirk2281337@gmail.com>
2024-06-15 14:41:02 +0500

    xd

--- frontend-server/src/components/services/api.ts
+++ frontend-server/src/components/services/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 
-export const BACKEND_URL = `http://localhost:81/api`;
+export const BACKEND_URL = `http://localhost:8000/api`;
 export const REQUEST_TIMEOUT = 5000000;
 
 export const createAPI = (): AxiosInstance => {
