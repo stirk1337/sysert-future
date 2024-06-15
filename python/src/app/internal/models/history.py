a6ffcbc8b958a6e04aa0737fd869ebbab67e15f4
Ramil <stirk2281337@gmail.com>
2024-06-15 14:54:55 +0500

    phsnxn

--- python/src/app/internal/models/history.py
+++ python/src/app/internal/models/history.py
@@ -1,11 +1,12 @@
+from ckeditor.fields import RichTextField
 from django.db import models
 
 
 class History(models.Model):
     title = models.CharField(max_length=500, verbose_name="Заголовок")
-    description = models.CharField(max_length=500, verbose_name="Описание")
+    description = RichTextField(verbose_name="Описание")
     date = models.CharField(max_length=500, verbose_name="Дата")
-    image_url = models.CharField(max_length=500, verbose_name="Ссылка на картинку")
+    image_url = RichTextField(verbose_name="Картинка")
 
     def __str__(self):
         return self.title
